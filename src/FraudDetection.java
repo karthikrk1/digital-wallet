9712ea120e76c9a1ef933b63b1685ef36aa98654
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2016-11-04 19:53:02 -0500

    Initial commit with all the necessary setup

---
+++ src/FraudDetection.java
@@ -0,0 +1,14 @@
+/**
+ * The following program is the solution for the Insight Data Engineering Fellowship Coding Challenge - Digital Wallet
+ * 
+ * @author Karthik Ramakrishnan
+ * 
+ */
+
+// Package imports 
+import java.util.*;
+
+// Main public class definition
+public class FraudDetection {
+
+}
